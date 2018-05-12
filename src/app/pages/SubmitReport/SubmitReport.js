704ab209b28a7de3c5786ab5889356d2c6024248
dunjamarc <dunjamarkovic86@gmail.com>
2018-05-12 14:28:15 +0200

    submit report page setup

---
+++ src/app/pages/SubmitReport/SubmitReport.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import '../../App.css';
+import Search from '../../partials/Search';
+
+class SubmitReport extends Component {
+
+    /*constructor (props){
+        super(props);
+        this.state = {
+            allReports: [],
+            inputValue: '',
+            search: []
+        }
+    }
+
+    componentDidMount(){
+        reportData.allReportsData()
+        .then(data => {
+            this.setState({
+                allReports: data,
+                search: data
+            })
+        })
+    }*/
+
+    render() {
+        return (
+            <div className='container'>
+                <div className="row">
+                    <div className="col l3 m3">
+                        <ul className="panel-nav">
+                            <li><div className="numberCircle valign-wrapper">1</div><h6 className="panel-heading">Select Candidate</h6></li>
+                            <li><div className="numberCircle">2</div><h6 className="panel-heading">Select Company</h6></li>
+                            <li><div className="numberCircle">3</div><h6 className="panel-heading">Fill Report Detail</h6></li>
+                        </ul>
+
+                    </div>
+                    <div className="col l9 m9 panel">
+                        <h6 className="panel-heading">Fill Report Detail</h6>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SubmitReport;
