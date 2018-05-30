2cec8ad96fb24354d8db075a9a8060620136e6f6
dunjamarc <dunjamarkovic86@gmail.com>
2018-05-30 16:21:59 +0200

    showSelection component

---
+++ src/app/pages/SubmitReport/ShowSelection.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import '../../App.css';
+
+const ShowSelection = (props) => {
+
+    return (
+
+        <div className="showSelection">
+            <p className="mail">Candidate:</p>
+            <h6>{sessionStorage.getItem('candidateName')}</h6>
+            {props.show === 'companies' ? '' :
+                <React.Fragment>
+                    <p className="mail">Company:</p>
+                    <h6>{sessionStorage.getItem('companyName')}</h6>
+                </React.Fragment>}
+        </div>
+
+    )
+
+}
+
+export default ShowSelection;
--- src/app/pages/SubmitReport/SubmitReport.js
+++ src/app/pages/SubmitReport/SubmitReport.js
@@ -3,6 +3,7 @@ import '../../App.css';
 import SelectCandidate from './SelectCandidate';
 import SelectCompany from './SelectCompany';
 import FillDetails from './FillDetails';
+import ShowSelection from './ShowSelection';
 
 class SubmitReport extends Component {
 
@@ -26,11 +27,17 @@ class SubmitReport extends Component {
                 <div className="row">
                     <div className="col l3 m3">
                         <ul className="panel-nav">
-                            <li><div className="numberCircle valign-wrapper">1</div><h6 className="panel-heading">Select Candidate</h6></li>
-                            <li><div className="numberCircle">2</div><h6 className="panel-heading">Select Company</h6></li>
-                            <li><div className="numberCircle">3</div><h6 className="panel-heading">Fill Report Detail</h6></li>
+                            <li className={this.state.show == 'candidates' ? 'active' : ''}>
+                                <div className="numberCircle valign-wrapper">1</div><h6 className="panel-heading">Select Candidate</h6>
+                            </li>
+                            <li className={this.state.show == 'companies' ? 'active' : ''}>
+                                <div className="numberCircle">2</div><h6 className="panel-heading">Select Company</h6>
+                            </li>
+                            <li className={this.state.show == 'reportDetails' ? 'active' : ''}>
+                                <div className="numberCircle">3</div><h6 className="panel-heading">Fill Report Detail</h6>
+                            </li>
                         </ul>
-
+                        {this.state.show == 'candidates' ? '' : <ShowSelection show={this.state.show}/>}
                     </div>
                     <div className="col l9 m9 panel">
                         {this.state.show == 'candidates' ? <SelectCandidate page={this.goToPage} /> : ''}
